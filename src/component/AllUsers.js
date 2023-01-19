13cc86ddea7d5c12342ff22ea524caad6425412c
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2023-01-19 15:17:11 +0600

    completed all function

--- src/component/AllUsers.js
+++ src/component/AllUsers.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Button, Modal, Table } from 'react-bootstrap';
+import { Button, Form, Modal, Table } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaTrash, FaUserEdit } from "react-icons/fa";
+import { toast, ToastContainer } from 'react-toastify';
+
+
+const nameReg = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/
 
 const AllUsers = () => {
     const navigate = useNavigate();
@@ -10,12 +14,22 @@ const AllUsers = () => {
     const [sectors, setSectors] = useState([])
     const [users, setUsers] = useState([])
     const [show, setShow] = useState(false);
+    const [newSectors, setNewSector] = useState()
+    const [error, setError] = useState()
+    const [newName, setNewName] = useState('')
+    const [id, setId] = useState('')
 
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+
+    const handleShow = (id) => {
+        setId(id)
+        setNewSector(users.find(u => u._id === id)?.sectors)
+        setNewName(users.find(u => u._id === id)?.name)
+        setShow(true);
+    }
+
     useEffect(() => {
-        setLoading(false)
-        axios.get('http://localhost:5000/sectors')
+        axios.get('https://inc-bernardine-abdulmomin956.koyeb.app/sectors')
             .then(res => setSectors(res.data))
             .catch(err => console.log(err))
 
@@ -24,22 +38,46 @@ const AllUsers = () => {
     useEffect(() => {
         if (loading) {
             setLoading(false)
-            axios.get('http://localhost:5000/users')
+            axios.get('https://inc-bernardine-abdulmomin956.koyeb.app/users')
                 .then(res => setUsers(res.data))
                 .catch(err => console.log(err))
         }
     }, [loading])
+
     const filtArr = [...sectors.flat(), ...sectors.map(s => s.sub.flat()).flat(), ...sectors.map(s => s.sub.map(sub => sub.sub2).flat()).flat(), ...sectors.map(s => s.sub.map(sub => sub?.sub2?.map(sub2 => sub2?.sub3).flat()).flat()).flat()];
 
     const definedArr = filtArr.filter(item => item);
     // console.log(definedArr);
 
     const handleDelete = (id) => {
-
+        axios.delete(`https://inc-bernardine-abdulmomin956.koyeb.app/user/${id}`)
+            .then(res => {
+                toast("Deleted")
+                setLoading(true)
+                setShow(false)
+            })
+            .catch(err => { console.log(err); setLoading(true); setShow(false) })
     }
+
+    const handleSave = (id) => {
+        if (nameReg.test(newName)) {
+            axios.patch(`https://inc-bernardine-abdulmomin956.koyeb.app/user/${id}`, { name: newName, sectors: newSectors })
+                .then(res => {
+                    toast("Updated")
+                    setLoading(true)
+                    setShow(false)
+                })
+                .catch(err => { console.log(err); setLoading(true); setShow(false) })
+        } else {
+            setError('Name is not valid')
+        }
+    }
+
+
+
     return (
         <div style={{ height: '100vh' }} className='d-flex flex-column justify-content-center align-items-center bg-light'>
-            <div className='w-25 d-flex flex-column'>
+            <div className='w-50 d-flex flex-column'>
                 <Button onClick={() => navigate('/')} className=''>Add a new user</Button>
                 <Table className='bg-white shadow-sm' responsive="sm">
                     <thead>
@@ -64,7 +102,7 @@ const AllUsers = () => {
                                 </button>
                                 <button
                                     className="btn btn-ghost btn-sm py-0"
-                                    onClick={handleShow}
+                                    onClick={() => handleShow(u._id)}
                                 >
                                     <FaUserEdit className="h4 text-primary mb-0 py-0"></FaUserEdit>
                                 </button>
@@ -74,19 +112,40 @@ const AllUsers = () => {
                 </Table>
                 <Modal show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
+                        <Modal.Title>Update User</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Body>
+                        <Form onSubmit={handleSave} className=' bg-white'>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control value={newName} onChange={e => { setNewName(e.target.value); setError('') }} type="text" placeholder="Enter name" required />
+                                {error && <small className='text-danger'>{error}</small>}
+                            </Form.Group>
+                            <Form.Label>Sectors</Form.Label>
+                            <Form.Select defaultValue={newSectors} onChange={e => setNewSector(e.target.value)} aria-label="Default select example" required>
+                                <option value='' disabled>Open this select menu</option>
+                                {
+                                    sectors.map(main => {
+                                        return <><option key={main.value} value={main.value}>{main.label}</option>
+                                            {main.sub ? main.sub.map(sub => <><option value={sub.value}>&nbsp;&nbsp;&nbsp;&nbsp;{sub.label}</option>
+                                                {sub.sub2 ? sub.sub2.map(sub2 => <><option value={sub2.value}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{sub2.label}</option>
+                                                    {sub2.sub3 ? sub2.sub3.map(sub3 => <option value={sub3.value}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{sub3.label}</option>) : ''}</>) : ''}</>) : ''} </>
+                                    })
+                                }
+                            </Form.Select>
+                        </Form>
+                    </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleClose}>
                             Close
                         </Button>
-                        <Button variant="primary" onClick={handleClose}>
+                        <Button variant="primary" onClick={() => handleSave(id)}>
                             Save Changes
                         </Button>
                     </Modal.Footer>
                 </Modal>
             </div>
+            <ToastContainer />
         </div>
     );
 };
--- src/component/Home.js
+++ src/component/Home.js
@@ -2,198 +2,53 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const nameReg = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/
 
 const Home = () => {
-    const [selectors, setSelectors] = useState([])
+    const [sectors, setSectors] = useState([])
     const [newName, setNewName] = useState('')
-    const [newSectors, setNewSector] = useState()
+    const [newSectors, setNewSector] = useState('')
     const [error, setError] = useState()
-    const [loading, setLoading] = useState(true)
     const navigate = useNavigate()
 
+
     useEffect(() => {
-        if (loading) {
-            setLoading(false)
-            axios.get('http://localhost:5000/sectors')
-                .then(res => setSelectors(res.data))
-                .catch(err => console.log(err))
-        }
-    }, [loading])
+        axios.get('https://inc-bernardine-abdulmomin956.koyeb.app/sectors')
+            .then(res => setSectors(res.data))
+            .catch(err => console.log(err))
+    }, [])
 
     const handleSave = (e) => {
         e.preventDefault();
         // console.log(newName, newSelectors);
         if (nameReg.test(newName)) {
-            axios.post('http://localhost:5000/users', { name: newName, sectors: newSectors, agree: true })
-                .then(res => setLoading(true))
-                .catch(err => { console.log(err); setLoading(true) })
+            axios.post('https://inc-bernardine-abdulmomin956.koyeb.app/users', { name: newName, sectors: newSectors, agree: true })
+                .then(res => {
+                    if (res.status === 200) {
+                        toast.success("User Added Successfully!", {
+                            position: "top-center",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                        })
+                        setNewName('')
+                        setNewSector('')
+                    }
+                })
+                .catch(err => { console.log(err); })
         } else {
             setError('Name is not valid')
         }
     }
 
-    // const selectors = [
-    //     {
-    //         value: "1",
-    //         label: 'Manufacturing',
-    //         sub: [
-    //             { value: "19", label: 'Construction materials' },
-    //             { value: "18", label: 'Electronics and Optics' },
-    //             {
-    //                 value: "6", label: 'Food and Beverage',
-    //                 sub2: [
-    //                     { value: "342", label: 'Bakery & confectionery products' },
-    //                     { value: "43", label: 'Beverages' },
-    //                     { value: "42", label: 'Fish & fish products' },
-    //                     { value: "40", label: 'Meat & meat products' },
-    //                     { value: "39", label: 'Milk & dairy products' },
-    //                     { value: "437", label: 'Other ' },
-    //                     { value: "378", label: 'Sweets & snack food' },
-    //                 ]
-    //             },
 
-    //             {
-    //                 value: "13", label: 'Furniture',
-    //                 sub2: [
-    //                     { value: "389", label: 'Bathroom / sauna' },
-    //                     { value: "385", label: 'Bedroom' },
-    //                     { value: "390", label: 'Children’s room' },
-    //                     { value: "98", label: 'Kitchen' },
-    //                     { value: "101", label: 'Living room' },
-    //                     { value: "392", label: 'Office' },
-    //                     { value: "394", label: 'Other(Furniture)' },
-    //                     { value: "341", label: 'Outdoor' },
-    //                     { value: "99", label: 'Project furniture' },
-    //                 ]
-    //             },
-
-    //             {
-    //                 value: "12", label: 'Machinery',
-    //                 sub2: [
-    //                     { value: "94", label: 'Machinery components' },
-    //                     { value: "91", label: 'Machinery equipment / tools' },
-    //                     { value: "224", label: 'Manufacture of machinery' },
-    //                     {
-    //                         value: "97", label: 'Maritime',
-    //                         sub3: [
-    //                             { value: "271", label: 'Aluminium and steel workboats' },
-    //                             { value: "269", label: 'Boat / Yacht building' },
-    //                             { value: "230", label: 'Ship repair and conversion' },
-    //                         ]
-    //                     },
-
-    //                     { value: "93", label: 'Metal structures' },
-    //                     { value: "508", label: 'Other' },
-    //                     { value: "227", label: 'Repair and maintenance service' },
-    //                 ]
-    //             },
-
-    //             {
-    //                 value: "11", label: 'Metalworking',
-    //                 sub2: [
-    //                     { value: "67", label: 'Construction of metal structures' },
-    //                     { value: "263", label: 'Houses and buildings' },
-    //                     { value: "267", label: 'Metal products' },
-    //                     {
-    //                         value: "542", label: 'Metal works',
-    //                         sub3: [
-    //                             { value: "75", label: 'CNC - machining' },
-    //                             { value: "62", label: 'Forgings, Fasteners' },
-    //                             { value: "69", label: 'Gas, Plasma, Laser cutting' },
-    //                             { value: "66", label: 'MIG, TIG, Aluminum welding' },
-    //                         ]
-    //                     },
-    //                 ]
-    //             },
-
-
-    //             {
-    //                 value: "9", label: 'Plastic and Rubber',
-    //                 sub2: [
-    //                     { value: "54", label: 'Packaging' },
-    //                     { value: "556", label: 'Plastic goods' },
-    //                     {
-    //                         value: "559", label: 'Plastic processing technology',
-    //                         sub3: [
-    //                             { value: "55", label: 'Blowing' },
-    //                             { value: "57", label: 'Moulding' },
-    //                             { value: "53", label: 'Plastics welding and processing' },
-    //                         ]
-    //                     },
-
-    //                     { value: "560", label: 'Plastic profiles' },
-    //                 ]
-    //             },
-
-    //             {
-    //                 value: "5", label: 'Printing',
-    //                 sub2: [
-    //                     { value: "148", label: 'Advertising' },
-    //                     { value: "150", label: 'Book / Periodicals printing' },
-    //                     { value: "145", label: 'Labelling and packaging printing' },
-    //                 ]
-    //             },
-
-    //             {
-    //                 value: "7", label: 'Textile and Clothing',
-    //                 sub2: [
-    //                     { value: "44", label: 'Clothing' },
-    //                     { value: "45", label: 'Textile' },
-    //                 ]
-    //             },
-
-    //             {
-    //                 value: "8", label: 'Wood',
-    //                 sub2: [
-    //                     { value: "337", label: 'Other(Wood)' },
-    //                     { value: "51", label: 'Wooden building materials' },
-    //                     { value: "47", label: 'Wooden houses' },
-    //                 ]
-    //             },
-
-    //         ]
-    //     },
-
-    //     {
-    //         value: "3", label: 'Other',
-    //         sub: [
-    //             { value: "37", label: 'Creative industries' },
-    //             { value: "29", label: 'Energy technology' },
-    //             { value: "33", label: 'Environment' },
-    //         ]
-    //     },
-
-    //     {
-    //         value: "2", label: 'Service',
-    //         sub: [
-    //             { value: "25", label: 'Business services' },
-    //             { value: "35", label: 'Engineering' },
-    //             {
-    //                 value: "28", label: 'Information Technology and Telecommunications',
-    //                 sub2: [
-    //                     { value: "581", label: 'Data processing, Web portals, E - marketing' },
-    //                     { value: "576", label: 'Programming, Consultancy' },
-    //                     { value: "121", label: 'Software, Hardware' },
-    //                     { value: "122", label: 'Telecommunications' },
-    //                 ]
-    //             },
-
-    //             { value: "22", label: 'Tourism' },
-    //             { value: "141", label: 'Translation services' },
-    //             {
-    //                 value: "21", label: 'Transport and Logistics',
-    //                 sub2: [
-    //                     { value: "111", label: 'Air' },
-    //                     { value: "114", label: 'Rail' },
-    //                     { value: "112", label: 'Road' },
-    //                     { value: "113", label: 'Water' },
-    //                 ]
-    //             },
-    //         ]
-    //     },
-    // ]
 
     return (
         <div style={{ height: '100vh' }} className='d-flex flex-column justify-content-center align-items-center bg-light'>
@@ -206,10 +61,10 @@ const Home = () => {
                         {error && <small className='text-danger'>{error}</small>}
                     </Form.Group>
                     <Form.Label>Sectors</Form.Label>
-                    <Form.Select onChange={e => setNewSector(e.target.value)} aria-label="Default select example" required defaultValue=''>
+                    <Form.Select onChange={e => setNewSector(e.target.value)} aria-label="Default select example" required value={newSectors}>
                         <option value='' disabled>Open this select menu</option>
                         {
-                            selectors.map(main => {
+                            sectors.map(main => {
                                 return <><option key={main.value} value={main.value}>{main.label}</option>
                                     {main.sub ? main.sub.map(sub => <><option value={sub.value}>&nbsp;&nbsp;&nbsp;&nbsp;{sub.label}</option>
                                         {sub.sub2 ? sub.sub2.map(sub2 => <><option value={sub2.value}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{sub2.label}</option>
@@ -225,6 +80,16 @@ const Home = () => {
                     </Button>
                 </Form>
             </div>
+            <ToastContainer position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light" />
         </div>
     );
 };
